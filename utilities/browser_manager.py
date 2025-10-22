1fb27117beaf457c334cb4b537a517a01d8cf2ce
Gaurav Kumar <gauravkumar07082001@gmail.com>
2025-10-22 13:03:11 +0530

    Added headless support for browsers2

--- utilities/browser_manager.py
+++ utilities/browser_manager.py
@@ -51,8 +51,47 @@
 
 
 
-# File: utilities/browser_manager.py
+# File: utilities/browser_manager.py-2  working any prob then rev -3
+#
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service as ChromeService
+# from selenium.webdriver.firefox.service import Service as FirefoxService
+# from selenium.webdriver.edge.service import Service as EdgeService
+#
+# from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.firefox import GeckoDriverManager
+# from webdriver_manager.microsoft import EdgeChromiumDriverManager
+#
+# def get_driver(browser="chrome"):
+#     browser = browser.lower()
+#
+#     if browser == "chrome":
+#         options = webdriver.ChromeOptions()
+#         options.add_argument("--disable-notifications")
+#         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+#
+#     elif browser == "firefox":
+#         options = webdriver.FirefoxOptions()
+#         options.set_preference("dom.webnotifications.enabled", False)
+#         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
+#
+#     elif browser == "edge":
+#         options = webdriver.EdgeOptions()
+#         options.add_argument("--disable-notifications")
+#         driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
+#
+#     else:
+#         raise Exception(f"Unsupported browser: {browser}")
+#
+#     # Common settings
+#     driver.set_page_load_timeout(15)   # Reduced page load timeout
+#     driver.implicitly_wait(5)          # Reduced implicit wait
+#     driver.maximize_window()
+#
+#     return driver
 
+
+#file -3
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.firefox.service import Service as FirefoxService
@@ -62,22 +101,33 @@ from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager
 
-def get_driver(browser="chrome"):
+def get_driver(browser="chrome", headless=False):
     browser = browser.lower()
 
+    # Initialize the driver options based on the browser type
     if browser == "chrome":
         options = webdriver.ChromeOptions()
         options.add_argument("--disable-notifications")
+        if headless:
+            options.add_argument("--headless")  # Add headless mode for Chrome
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--window-size=1920x1080")  # Optional for fixed size
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
     elif browser == "firefox":
         options = webdriver.FirefoxOptions()
         options.set_preference("dom.webnotifications.enabled", False)
+        if headless:
+            options.add_argument("--headless")  # Add headless mode for Firefox
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 
     elif browser == "edge":
         options = webdriver.EdgeOptions()
         options.add_argument("--disable-notifications")
+        if headless:
+            options.add_argument("--headless")  # Add headless mode for Edge
         driver = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()), options=options)
 
     else:
